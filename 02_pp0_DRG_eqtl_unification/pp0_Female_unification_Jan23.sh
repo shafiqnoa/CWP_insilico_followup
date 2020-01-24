f5b64ab2da2906ab41f0022b402da8f9a0c59412
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-25 00:26:17 +0700

    made sex specific mapping file

--- 02_pp0_DRG_eqtl_unification/pp0_Female_unification_Jan23.sh
+++ 02_pp0_DRG_eqtl_unification/pp0_Female_unification_Jan23.sh
@@ -1,9 +1,9 @@
 run_uni_qc_rep \
 --gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtlmerged_F.csv \
---mapping-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtl_mapping.json \
---descriptors-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtl_descriptors.json \
+--mapping-path=./pp0_eqtl_mapping_F.json \
+--descriptors-path=./pp0_eqtl_descriptors.json \
 --output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/eqtl_uni_out_Female \
 --output-file=pp0_Female_unification \
---filter-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_filter.txt \
+--filter-path=./pp0_filter.txt \
 --eqtl \
 --qc-report
--- 02_pp0_DRG_eqtl_unification/pp0_Male_unification_Jan23.sh
+++ 02_pp0_DRG_eqtl_unification/pp0_Male_unification_Jan23.sh
@@ -1,9 +1,9 @@
 run_uni_qc_rep \
 --gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtlmerged_M.csv \
---mapping-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtl_mapping.json \
---descriptors-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtl_descriptors.json \
+--mapping-path=./pp0_eqtl_mapping_M.json \
+--descriptors-path=./pp0_eqtl_descriptors.json \
 --output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/eqtl_uni_out_Male \
 --output-file=pp0_Male_unification \
---filter-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_filter.txt \
+--filter-path=./pp0_filter.txt \
 --eqtl \
 --qc-report
