3070c0a78d305997445b02cdebdde1f3caf3f609
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-24 18:22:03 +0700

    Renamed file name

---
+++ 01_CWP_GWAS_unification/01_cwp_unification.sh
@@ -0,0 +1,8 @@
+run_uni_qc_rep \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/data/CWP_GWAS.txt \
+--mapping-path=/home/ubuntu/polyomica/projects/CWP_project/data/mapping_CWP.json \
+--descriptors-path=/home/ubuntu/polyomica/projects/CWP_project/data/descriptor.txt \
+--output-dir=/home/ubuntu/polyomica/projects/CWP_project/cwp_output_test \
+--output-file=cwp_unification_test \
+--filter-path=/home/ubuntu/polyomica/projects/CWP_project/data/filter_cwp_gwas.txt \
+--qc-report
