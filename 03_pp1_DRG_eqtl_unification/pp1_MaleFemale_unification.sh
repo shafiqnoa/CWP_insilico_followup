ef65b82c63e4911e268f0e578971931f020aec37
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-25 20:23:27 +0700

    new path defined in the file

--- 03_pp1_DRG_eqtl_unification/pp1_MaleFemale_unification.sh
+++ 03_pp1_DRG_eqtl_unification/pp1_MaleFemale_unification.sh
@@ -1,9 +1,9 @@
 run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp1_eqtlmerged_MF.csv \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp1_eqtlmerged_MF.csv \
 --mapping-path=./pp1_eqtl_mapping_MF.json \
 --descriptors-path=./pp1_eqtl_descriptors.json \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/eqtl_uni_output \
+--output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp1_MF_output \
 --output-file=pp1_MF_unification \
 --filter-path=./pp1_filter.txt \
---eqtl \
---qc-report
+--qc-report \
+--eqtl
