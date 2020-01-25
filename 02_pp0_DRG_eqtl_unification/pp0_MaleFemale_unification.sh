ccfe86d1fc9960aa1c7bcf56346c2b3aabbf77ed
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-25 19:47:29 +0700

    files update with new path

--- 02_pp0_DRG_eqtl_unification/pp0_MaleFemale_unification.sh
+++ 02_pp0_DRG_eqtl_unification/pp0_MaleFemale_unification.sh
@@ -1,8 +1,8 @@
 run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp0_eqtlmerged_MF.csv \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp0_eqtlmerged_MF.csv \
 --mapping-path=./pp0_eqtl_mapping_MF.json \
 --descriptors-path=./pp0_eqtl_descriptors.json \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/eqtl_uni_output \
+--output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output \
 --output-file=pp0_MF_unification \
 --filter-path=./pp0_filter.txt \
 --qc-report \
