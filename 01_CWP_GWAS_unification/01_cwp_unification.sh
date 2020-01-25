f2bf554329398ab8126bbd402ab5c3946e964add
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-25 21:19:48 +0700

    data path and file name updated

--- 01_CWP_GWAS_unification/01_cwp_unification.sh
+++ 01_CWP_GWAS_unification/01_cwp_unification.sh
@@ -1,8 +1,8 @@
 run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/data/CWP_GWAS.txt \
---mapping-path=/home/ubuntu/polyomica/projects/CWP_project/data/mapping_CWP.json \
---descriptors-path=/home/ubuntu/polyomica/projects/CWP_project/data/descriptor.txt \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/OUTPUT \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_MAIN/CWP_GWAS.txt \
+--mapping-path=./01_cwp_mapping.json \
+--descriptors-path=./01_cwp_descriptors.txt \
+--output-dir=/home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_UNIFIED \
 --output-file=cwp_unification_output \
---filter-path=/home/ubuntu/polyomica/projects/CWP_project/data/filter_cwp_gwas.txt \
+--filter-path=./01_cwp_filter.txt \
 --qc-report
