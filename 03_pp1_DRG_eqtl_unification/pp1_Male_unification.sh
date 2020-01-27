49c354f1ce30193b34f023c7f933e9c99a5ddae8
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-27 20:24:50 +0700

    delete

--- 03_pp1_DRG_eqtl_unification/pp1_Male_unification.sh
+++
@@ -1,9 +0,0 @@
-run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp1_eqtlmerged_M.csv \
---mapping-path=./pp1_eqtl_mapping_M.json \
---descriptors-path=./pp1_eqtl_descriptors.json \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp1_M_output \
---output-file=pp1_M_unification \
---filter-path=./pp1_filter.txt \
---qc-report \
---eqtl
