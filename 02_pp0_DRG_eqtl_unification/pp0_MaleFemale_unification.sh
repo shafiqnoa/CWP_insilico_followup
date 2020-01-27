561772d1494e571a9a08cd7fb9d54d3e979206b3
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-27 20:12:21 +0700

    older files delete

--- 02_pp0_DRG_eqtl_unification/pp0_MaleFemale_unification.sh
+++
@@ -1,9 +0,0 @@
-run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp0_eqtlmerged_MF.csv \
---mapping-path=./pp0_eqtl_mapping_MF.json \
---descriptors-path=./pp0_eqtl_descriptors.json \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output \
---output-file=pp0_MF_unification \
---filter-path=./pp0_filter.txt \
---qc-report \
---eqtl
--- 02_pp0_DRG_eqtl_unification/pp0_Male_unification_Jan23.sh
+++
@@ -1,9 +0,0 @@
-run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp0_eqtlmerged_M.csv \
---mapping-path=./pp0_eqtl_mapping_M.json \
---descriptors-path=./pp0_eqtl_descriptors.json \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_M_output \
---output-file=pp0_M_unification \
---filter-path=./pp0_filter.txt \
---qc-report \
---eqtl
