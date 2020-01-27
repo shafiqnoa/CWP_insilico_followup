fcbb976bd4b73179c78c4b8430f7769ffecf9074
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-27 20:01:59 +0700

    chnaged file name and updated .sh file

--- 01_CWP_GWAS_unification/01_cwp_unification.sh
+++ 01_CWP_GWAS_unification/01_cwp_unification.sh
@@ -1,8 +1,8 @@
 run_uni_qc_rep \
 --gwas-path=/home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_MAIN/CWP_GWAS.txt \
---mapping-path=./01_cwp_mapping.json \
---descriptors-path=./01_cwp_descriptors.txt \
+--mapping-path=./cwp_mapping.json \
+--descriptors-path=./cwp_descriptors.txt \
 --output-dir=/home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_UNIFIED \
 --output-file=cwp_unification_output \
---filter-path=./01_cwp_filter.txt \
+--filter-path=./cwp_filter.txt \
 --qc-report
