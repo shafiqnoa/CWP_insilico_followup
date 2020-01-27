21d3d4866a07c01f94010d406548e3b7c0a6e3dd
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-27 20:19:01 +0700

    .sh file name updated

---
+++ 03_pp1_DRG_eqtl_unification/01_pp1_Female_unification.sh
@@ -0,0 +1,9 @@
+run_uni_qc_rep \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp1_eqtlmerged_F.csv \
+--mapping-path=./pp1_eqtl_mapping_F.json \
+--descriptors-path=./pp1_eqtl_descriptors.json \
+--output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp1_F_output \
+--output-file=pp1_F_unification \
+--filter-path=./pp1_filter.txt \
+--qc-report \
+--eqtl
--- 03_pp1_DRG_eqtl_unification/pp1_MF_unification_Jan23.sh
+++
@@ -1,9 +0,0 @@
-run_uni_qc_rep \
---gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp1_eqtlmerged_MF.csv \
---mapping-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp1_eqtl_mapping.json \
---descriptors-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp1_eqtl_descriptors.json \
---output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/eqtl_uni_output \
---output-file=pp1_MF_unification \
---filter-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_eqtl_merged/pp1_filter.txt \
---eqtl \
---qc-report
