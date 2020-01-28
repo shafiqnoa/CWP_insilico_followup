7f9b047655d85d95bc11b28c7624d3fb1fea57e3
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-28 20:07:17 +0700

    .csv added

--- 06_HMR_HEIDI/01_hmrdeidi_complextraits.sh
+++ 06_HMR_HEIDI/01_hmrdeidi_complextraits.sh
@@ -11,6 +11,6 @@ echo $gid
 run_smrheidi
 --gwas-id-1 1287006 \
 --gwas-id-2 $gid \
---snp-list=~/polyomica/projects/CWP_project/CWP_DATA_MAIN/snplist.txt \
+--snp-list=~/polyomica/projects/CWP_project/CWP_DATA_MAIN/snplist.csv \
 --version=1
 done
