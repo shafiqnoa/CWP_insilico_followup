722c02a941a1ec1b0877857df69059c46ecca914
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-29 20:13:58 +0700

    filters in the scripts modified

--- 04_Rscripts/01_chr_merged_prep.R
+++ 04_Rscripts/01_chr_merged_prep.R
@@ -1,3 +1,8 @@
+library("data.table")
+library("R.utils")
+
+
+
 # reading files with SNPs from all 22 chromosomes
 setwd("~/polyomica/projects/DRG_eQTLs/01_raw_data/eQTLs/rs_freq_information")
 all.files <- list.files()
@@ -5,8 +10,7 @@ l <- lapply(all.files, fread, sep="\t")
 chr_merged <- rbindlist(l)
 colnames(chr_merged )[1]<- "RSID"
 dim(chr_merged) # 81653337; 7
-chr_merged <- chr_merged[chr_merged$MAF>=0.01,] 
-chr_merged <- chr_merged[chr_merged$NCHROBS>=(max(chr_merged$NCHROBS)*0.95),]
-chr_merged <- chr_merged[which(!duplicated(chr_merged$RSID)),]
-dim(chr_merged) #8145704; 7
+chr_merged <- chr_merged[chr_merged$MAF>0,] #18378218 snps (getting rid of monomorphic snps)
+chr_merged <- chr_merged[chr_merged$NCHROBS>=(max(chr_merged$NCHROBS)*0.95),] # 16670506 snps
+chr_merged <- chr_merged[which(!duplicated(chr_merged$RSID)),] # 16602154 snps left.
 fwrite(chr_merged, file="~/polyomica/projects/CWP_project/DRG_DATA_MERGED/chr_merged.txt")
