d3d5432d595b26e419ed2690c49845f04fe340dc
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-29 18:40:27 +0700

    descriptors file name updated in the scripts

--- 03_pp1_DRG_eqtl_unification/01_pp1_MaleFemale_unification.sh
+++ 03_pp1_DRG_eqtl_unification/01_pp1_MaleFemale_unification.sh
@@ -1,7 +1,7 @@
 run_uni_qc_rep \
 --gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_MERGED/pp1_eqtlmerged_MF.csv \
 --mapping-path=./pp1_eqtl_mapping_MF.json \
---descriptors-path=./pp1_eqtl_descriptors.json \
+--descriptors-path=./pp1_eqtl_descriptors_MF.json \
 --output-dir=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp1_MF_output \
 --output-file=pp1_MF_unification \
 --filter-path=./pp1_filter.txt \
