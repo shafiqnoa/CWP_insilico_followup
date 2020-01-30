4acd8445d49ad1b4ca3d8dc61b5e47479d2af5a0
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-30 23:04:43 +0700

    script to downlaod smrheidi complex traits res

---
+++ 06_HMR_HEIDI/03_smrheidi_comtraits_report.sh
@@ -0,0 +1,8 @@
+export PROD=TRUE
+run_smrheidi_report \
+--gwas-id-1 1287006 \
+--version 1 \
+--output-dir ~/polyomica/projects/CWP_project/SMRHEIDI_COM_TRAITS \
+--output-file cwp_smrheidi_comtraits \
+--only-table
+done
