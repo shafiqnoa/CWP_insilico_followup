e91799db44827252496646a5c0cb72ab10be9d23
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-31 23:26:29 +0700

    modified

--- 07_VEP_ANALYSIS/01_tagsnps_10K_pl.sh
+++ 07_VEP_ANALYSIS/01_tagsnps_10K_pl.sh
@@ -3,7 +3,7 @@ do
 ./plink  \
 --bfile /home/ubuntu/polyomica/gwas/reference/by_chr/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes_nodup \
 --list-all \
---show-tags /home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_MAIN/cwp_index_snps.txt \
+--show-tags /home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_MAIN/cwp_index_snp.txt \
 --tag-r2 0.8 \
 --out /home/ubuntu/polyomica/projects/CWP_project/VEP/tagsnps_pl
 done
