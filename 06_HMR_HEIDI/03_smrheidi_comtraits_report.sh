a2685d1395470ec838eab2932d058d10e3b1c5b0
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-01-31 17:19:56 +0700

    modified

--- 06_HMR_HEIDI/03_smrheidi_comtraits_report.sh
+++ 06_HMR_HEIDI/03_smrheidi_comtraits_report.sh
@@ -2,6 +2,6 @@ export PROD=TRUE
 run_smrheidi_report \
 --gwas-id-1 1287006 \
 --version 1 \
---output-dir home/ubuntu/polyomica/projects/CWP_project/SMRHEIDI_COM_TRAITS \
+--output-dir /home/ubuntu/polyomica/projects/CWP_project/SMRHEIDI_COM_TRAITS \
 --output-file cwp_smrheidi_comtraits \
 --only-table
