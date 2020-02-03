d03e927e60cc2325c5233159985946fb59c912f7
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-02-03 23:33:17 +0700

    version 2

---
+++ 06_HMR_HEIDI/01a_hmrheidi_complextraits.sh
@@ -0,0 +1,15 @@
+export PROD=TRUE
+
+gs_id=$(cat /home/ubuntu/polyomica/projects/CWP_project/DESCRIPTORS/complex_gwas.csv)
+
+for gid in $gs_id
+do
+echo $gid
+
+run_smrheidi \
+--gwas-id-1 1287006 \
+--gwas-id-2 $gid \
+--snp-list /home/ubuntu/polyomica/projects/CWP_project/CWP_DATA_MAIN/snplist.csv \
+--version 2 \
+--overwrite
+done
