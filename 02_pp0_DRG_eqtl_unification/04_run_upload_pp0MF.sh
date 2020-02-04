ec12e7812a959f5bd10c6ee6d16e77f910972a01
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-02-05 01:17:06 +0700

    run uplaod pp0MF

---
+++ 02_pp0_DRG_eqtl_unification/04_run_upload_pp0MF.sh
@@ -0,0 +1,33 @@
+export PROD=TRUE
+
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_0_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_1_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_2_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_3_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_4_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_5_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_6_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_7_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_8_done.csv
+
+run_upload \
+--gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_9_done.csv
+
