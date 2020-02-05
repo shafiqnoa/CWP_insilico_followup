382b3d830917b10bbb2c4682a64acafabb2af978
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-02-06 00:39:49 +0700

    updated

--- 06_HMR_HEIDI/02_hmrheidi_eqtlgwas.sh
+++ 06_HMR_HEIDI/02_hmrheidi_eqtlgwas.sh
@@ -1,6 +1,6 @@
 export PROD=TRUE
 
-gs_id=$(cat ~/polyomica/projects/CWP_project/DESCRIPTORS/eqtl_gwas.csv)
+gs_id=$(cat ~/polyomica/projects/CWP_project/SMRHEIDI_eQTLS/rs1491985.txt)
 
 for gid in $gs_id
 do
@@ -10,5 +10,5 @@ run_smrheidi \
 --gwas-id-1 1287006 \
 --gwas-id-2 $gid \
 --snp-list ~/polyomica/projects/CWP_project/CWP_DATA_MAIN/snplist.csv \
---version 1
+--version 2
 done
