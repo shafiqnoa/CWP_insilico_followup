417e5fa1aef4298402a57a6f0beae4ff8baf7db6
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-02-06 00:57:06 +0700

    scripts added for each snps

---
+++ 06_HMR_HEIDI/02b_hmrheidi_eqtlgwas.sh
@@ -0,0 +1,14 @@
+export PROD=TRUE
+
+gs_id=$(cat ~/polyomica/projects/CWP_project/SMRHEIDI_eQTLs/rs165599.txt)
+
+for gid in $gs_id
+do
+echo $gid
+
+run_smrheidi \
+--gwas-id-1 1287006 \
+--gwas-id-2 $gid \
+--snp-list ~/polyomica/projects/CWP_project/CWP_DATA_MAIN/snplist.csv \
+--version 2
+done
