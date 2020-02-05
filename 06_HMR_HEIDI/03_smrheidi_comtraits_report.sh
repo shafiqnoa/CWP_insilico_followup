6cadc8200ea8da821807fa4253cf59de918e0585
Shafiqur Md Rahman <shafiq@gwas-master.mga.local>
2020-02-05 17:36:23 +0700

    deleted versin flag

--- 06_HMR_HEIDI/03_smrheidi_comtraits_report.sh
+++ 06_HMR_HEIDI/03_smrheidi_comtraits_report.sh
@@ -1,7 +1,6 @@
 export PROD=TRUE
 run_smrheidi_report \
 --gwas-id-1 1287006 \
-#--version 2 \
 --output-dir /home/ubuntu/polyomica/projects/CWP_project/SMRHEIDI_COM_TRAITS \
 --output-file cwp_smrheidi_comtraits \
 --only-table
