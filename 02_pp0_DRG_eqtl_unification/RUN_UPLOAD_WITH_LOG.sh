fb296b0849b2892a2bc4814da80cec4b962a9dba
Md Rahman Shafiqur <shafiq@gwas-master.mga.local>
2020-02-12 18:24:26 +0700

    updated

--- 02_pp0_DRG_eqtl_unification/RUN_UPLOAD_WITH_LOG.sh
+++ 02_pp0_DRG_eqtl_unification/RUN_UPLOAD_WITH_LOG.sh
@@ -1,8 +1,9 @@
-for gid in 0 1 2 3 4 5 6 7 8 9 10
+for gid in 0 1 2 3 4 5 6 7 8 9
 do
 echo $gid
 run_upload \
 --gwas-path=/home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/pp0_MF_unification_"$gid"_done.csv \
+--eqtl \
 2>&1 | tee /home/ubuntu/polyomica/projects/CWP_project/DRG_DATA_UNIFIED/pp0_MF_output/log_upload_"$gid".txt
 
 done
